b7cfb59103c42c60beb423d058c90741dab78a7f
한민혁 <88497297+hazziu@users.noreply.github.com>
2024-07-31 15:00:46 +0900

    solve : [BOJ] 세로읽기_240728

---
+++ 5week/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		char[][] inputArr = new char[5][15];
+		for (int i = 0; i < 5; i++) {
+			String inputChar = sc.next();
+			for (int j = 0; j < inputChar.length(); j++) {
+
+				inputArr[i][j] = inputChar.charAt(j);
+			}
+		}
+
+
+		
+		StringBuilder result = new StringBuilder();
+
+		
+		loop: 
+		for (int j = 0; j < 15; j++) {
+
+			for (int i = 0; i < 5; i++) {
+
+				if (inputArr[i][j] != '\0') {
+
+					result.append(inputArr[i][j]);
+
+				}
+			}
+		}
+		System.out.println(result.toString());
+
+	}
+}
